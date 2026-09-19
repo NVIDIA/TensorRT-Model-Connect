0968758b7480991bdc384e47cff02e92547b8df3
chaofengw <chaofengw@nvidia.com>
2026-09-19 09:05:06 +0000

    fix(fast-foundation-stereo): recognize official checkpoint
    Signed-off-by: chaofengw <chaofengw@nvidia.com>

---
+++ families/fast_foundation_stereo/tests/test_support.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from tensorrt_model_connect.model_support import ModelMetadata
+
+from families.fast_foundation_stereo.support import describe
+
+
+def test_official_checkpoint_without_hugging_face_config_is_supported() -> None:
+    support = describe(ModelMetadata({}, {}, ("model_best_bp2_serialize.pth",)))
+
+    assert support is not None
+    assert support.default_task == "stereo_disparity"
+
+
+def test_unrelated_rootless_checkpoint_is_not_supported() -> None:
+    assert describe(ModelMetadata({}, {}, ("model.pt",))) is None
